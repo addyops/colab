3eb28fcb6dccee0e105f9b4d0dab5952a4e74f78
Trojanscript <64583763+addy005@users.noreply.github.com>
2024-05-06 02:34:44 +0530

    Update core.py

--- facefusion/uis/core.py
+++ facefusion/uis/core.py
@@ -73,7 +73,7 @@ def register_ui_component(component_name : ComponentName, component: Component) 
 
 def launch() -> None:
 	ui_layouts_total = len(facefusion.globals.ui_layouts)
-	with gradio.Blocks(theme = get_theme(), css = get_css(), title = metadata.get('name') + ' ' + metadata.get('version')) as ui:
+	with gradio.Blocks(theme=get_theme(), css=get_css(), title=metadata.get('name') + ' ' + metadata.get('version'), share=True) as ui:
 		for ui_layout in facefusion.globals.ui_layouts:
 			ui_layout_module = load_ui_layout_module(ui_layout)
 			if ui_layout_module.pre_render():
